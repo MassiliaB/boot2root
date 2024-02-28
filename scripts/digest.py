ef5f68a5d3f7caa06afc2aa90c713d614dec43e9
Massilia <massblri@gmail.com>
2024-02-28 19:43:35 +0100

    cleaned

--- scripts/digest.py
+++ scripts/digest.py
@@ -2,16 +2,10 @@ import hashlib
 
 word = "SLASH"
 
-# SHA-256
-sha256_hashed_word = hashlib.sha256(word.encode()).hexdigest()
+sha256 = hashlib.sha256(word.encode()).hexdigest()
+md5 = hashlib.md5(word.encode()).hexdigest()
+sha1 = hashlib.sha1(word.encode()).hexdigest()
 
-# MD5
-md5_hashed_word = hashlib.md5(word.encode()).hexdigest()
-
-# SHA-1
-sha1_hashed_word = hashlib.sha1(word.encode()).hexdigest()
-
-print("Original word:", word)
-print("SHA-256 Hashed word:", sha256_hashed_word)
-print("MD5 Hashed word:", md5_hashed_word)
-print("SHA-1 Hashed word:", sha1_hashed_word)
+print("SHA-256:", sha256)
+print("MD5:", md5)
+print("SHA-1:", sha1)
--- scripts/turtle_script.py
+++ scripts/turtle_script.py
@@ -1,47 +1,26 @@
 import turtle
 
-# Initialisation du module Turtle
 turtle.setup(800, 600)
 window = turtle.Screen()
 t = turtle.Turtle()
 
-# Fonction pour lire et exécuter les commandes du fichier texte
 def execute_commands(filename):
-    # Lecture du fichier
     with open(filename, 'r') as file:
-        # Parcourir chaque ligne du fichier
         for line in file:
-            # Diviser la ligne en mots
             words = line.split()
-            # Vérifier chaque mot
             for i in range(len(words)):
-                # Si le mot est "Tourne gauche"
                 if words[i] == "Tourne" and words[i+1] == "gauche":
-                    # Récupérer le nombre de degrés
                     degrees = int(words[i+3])
-                    # Tourner à gauche du nombre de degrés spécifié
                     t.left(degrees)
-                # Si le mot est "Tourne droite"
                 elif words[i] == "Tourne" and words[i+1] == "droite":
-                    # Récupérer le nombre de degrés
                     degrees = int(words[i+3])
-                    # Tourner à droite du nombre de degrés spécifié
                     t.right(degrees)
-                # Si le mot est "Avance"
                 elif words[i] == "Avance":
-                    # Récupérer le nombre de pas
                     steps = int(words[i+1])
-                    # Avancer du nombre de pas spécifié
                     t.forward(steps)
-                # Si le mot est "Recule"
                 elif words[i] == "Recule":
-                    # Récupérer le nombre de pas
                     steps = int(words[i+1])
-                    # Reculer du nombre de pas spécifié
                     t.backward(steps)
 
-# Appeler la fonction avec le nom du fichier en argument
 execute_commands("turtle")
-
-# Attendre un clic pour fermer la fenêtre
 window.mainloop()
